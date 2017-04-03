4d96eabc400db85864c847c82ee2fcc03ee847aa
dstockton <david.stockton@moneysupermarket.com>
2017-04-03 17:03:44 +0100

    Update MaskPasswordsBuildWrapper.java

--- src/main/java/com/michelin/cio/hudson/plugins/maskpasswords/MaskPasswordsBuildWrapper.java
+++ src/main/java/com/michelin/cio/hudson/plugins/maskpasswords/MaskPasswordsBuildWrapper.java
@@ -238,11 +238,22 @@ public final class MaskPasswordsBuildWrapper extends SimpleBuildWrapper {
             this.password = Secret.fromString(password);
         }
 
+        /**
+         * Allow construction from a Secret object - this allows deep cloning without the overhead of an
+         * attempted (and failed) decryption cycle which throws an exception and the related overhead which
+         * can cause a massive overhead on an instance with many global mask passwords and jobs (especially
+         * when combined with the BuildPipeline plugin).
+         */
+        public VarPasswordPair(String var, Secret password) {
+            this.var = var;
+            this.password = password;
+        }
+
         @Override
         @SuppressFBWarnings(value = "CN_IDIOM_NO_SUPER_CALL", justification = "We do not expect anybody to use this class."
                 + "If they do, they must override clone() as well")
         public Object clone() {
-            return new VarPasswordPair(getVar(), getPassword());
+            return new VarPasswordPair(getVar(), new Secret(getPassword()));
         }
 
         @Override
